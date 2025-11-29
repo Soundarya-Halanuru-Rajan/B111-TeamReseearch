b51659a944425590c2f9671c72747427cf53afa3
Muhammad-Farooqi <mf24acy@herts.ac.uk>
2025-11-29 17:44:30 +0000

    removing repetitive code.

--- visualization-analysis.R
+++ visualization-analysis.R
@@ -8,19 +8,7 @@ colnames(X2016)[7] <- "GDP"
 #cleaning happiness null values in the dataset to reduce the errors
 
 Cleaned_Happiness <- na.omit(X2016$Happiness)
-print(head(Cleaned_Happiness))
-library(readr)
-X2016<-read_csv("2016.csv")
-
-#renaming the coloumns to bit shorter version
-colnames(X2016)[4] <-"Happiness"
-colnames(X2016)[7] <- "GDP"
-
-#cleaning happiness null values in the dataset to reduce the errors
-
-Cleaned_Happiness <- na.omit(X2016$Happiness)
 Cleaned_GDP <- na.omit(X2016$GDP)
-print(length(Cleaned_Happiness))
 print(length(Cleaned_GDP))
 
 
