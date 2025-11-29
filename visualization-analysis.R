5f54c2d2ff963301ae03ec5f1d2c427d03e76779
Muhammad-Farooqi <mf24acy@herts.ac.uk>
2025-11-29 16:29:18 +0000

    Creating a histogram of happiness data.

--- visualization-analysis.R
+++ visualization-analysis.R
@@ -9,3 +9,23 @@ colnames(X2016)[7] <- "GDP"
 
 Cleaned_Happiness <- na.omit(X2016$Happiness)
 print(head(Cleaned_Happiness))
+library(readr)
+X2016<-read_csv("2016.csv")
+
+#renaming the coloumns to bit shorter version
+colnames(X2016)[4] <-"Happiness"
+colnames(X2016)[7] <- "GDP"
+
+#cleaning happiness null values in the dataset to reduce the errors
+
+Cleaned_Happiness <- na.omit(X2016$Happiness)
+print(head(Cleaned_Happiness))
+hist(Cleaned_Happiness,
+     freq = FALSE,
+     main = "Histogram of Happiness Scores",
+     xlab = "Happiness Score")
+m <- mean(Cleaned_Happiness)
+s <- sd(Cleaned_Happiness)
+
+curve(dnorm(x, mean=m, sd=s),
+      col="blue", lwd=2, add= TRUE)
